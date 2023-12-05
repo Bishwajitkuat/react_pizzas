0b6491a4074e8ff29e748967e9031a924a91c4d4
Bishwajitkuat <bishwajitkuat@gmail.com>
2023-12-05 22:34:21 +0200

    router is created and different routes are added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home";
+import Menu from "./components/menu/Menu";
+import Cart from "./components/cart/Cart";
+import CreateOrder from "./components/order/CreateOrder";
+import Order from "./components/order/Order";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+]);
+
 function App() {
   return (
     <>
-      <h1 className="text-[5rem] text-red-400">Hello from react pizzas</h1>
+      <RouterProvider router={router} />
     </>
   );
 }
