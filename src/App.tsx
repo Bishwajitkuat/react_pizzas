f977a37687be35bd194011d2aa132a6d3372bb4b
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-01-07 22:46:08 +0200

    The root element of the router object is Layout compoent We can pass children elements into Layout compoent Depending on route the children elements to Layout compoent is determined.

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,33 @@ import Menu from "./components/menu/Menu";
 import Cart from "./components/cart/Cart";
 import CreateOrder from "./components/order/CreateOrder";
 import Order from "./components/order/Order";
+import Layout from "./components/Layout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/menu",
-    element: <Menu />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-  {
-    path: "/order/new",
-    element: <CreateOrder />,
-  },
-  {
-    path: "/order/:orderId",
-    element: <Order />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
   },
 ]);
 
