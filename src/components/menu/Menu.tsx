14a6dc09a639295c63e68325631f1607ceed8f83
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-01-07 23:43:56 +0200

    menuLoader function is defined. it calles getMenue function to get prizza menues from api and retrun it.

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,3 +1,11 @@
+import { MenuType, getMenu } from "../../lib/restaurant_api";
+
 export default function Menu() {
   return <div>Menu</div>;
 }
+
+// loader fucntion which fetch data and return data, these data can be as a value of loader property of route object.
+export const menuLoader = async (): Promise<MenuType[]> => {
+  const menus = await getMenu();
+  return menus;
+};
