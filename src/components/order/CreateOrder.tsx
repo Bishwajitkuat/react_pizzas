4709ee7768a4fd8941de74f30c5f8e7fe1b2b225
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-02 01:52:28 +0200

    This component is rendering a form to taking order, upon submission, it makes a POST request to /order/new route along with cart array.

--- src/components/order/CreateOrder.tsx
+++ src/components/order/CreateOrder.tsx
@@ -1,3 +1,84 @@
+import { Form } from "react-router-dom";
+
+// https://uibakery.io/regex-library/phone-number
+const isValidPhone = (str) =>
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str
+  );
+
+const fakeCart = [
+  {
+    pizzaId: 12,
+    name: "Mediterranean",
+    quantity: 2,
+    unitPrice: 16,
+    totalPrice: 32,
+  },
+  {
+    pizzaId: 6,
+    name: "Vegetale",
+    quantity: 1,
+    unitPrice: 13,
+    totalPrice: 13,
+  },
+  {
+    pizzaId: 11,
+    name: "Spinach and Mushroom",
+    quantity: 1,
+    unitPrice: 15,
+    totalPrice: 15,
+  },
+];
+
 export default function CreateOrder() {
-  return <div>CreateOrder</div>;
+  // const [withPriority, setWithPriority] = useState(false);
+  const cart = fakeCart;
+
+  return (
+    <div>
+      <h2>Ready to order? Let's go!</h2>
+
+      <Form action="/order/new" method="POST">
+        <div>
+          <label>First Name</label>
+          <input type="text" name="customer" required />
+        </div>
+
+        <div>
+          <label>Phone number</label>
+          <div>
+            <input type="tel" name="phone" required />
+          </div>
+        </div>
+
+        <div>
+          <label>Address</label>
+          <div>
+            <input type="text" name="address" required />
+          </div>
+        </div>
+
+        <div>
+          <input
+            type="checkbox"
+            name="priority"
+            id="priority"
+            // value={withPriority}
+            // onChange={(e) => setWithPriority(e.target.checked)}
+          />
+          <label htmlFor="priority">Want to yo give your order priority?</label>
+        </div>
+
+        <div>
+          <input
+            type="hidden"
+            id="cart"
+            name="cart"
+            value={JSON.stringify(cart)}
+          />
+          <button>Order now</button>
+        </div>
+      </Form>
+    </div>
+  );
 }
