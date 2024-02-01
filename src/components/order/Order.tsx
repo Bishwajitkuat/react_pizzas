d8666b839af658283d6313164d845216bb3cf88e
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-01 18:49:39 +0200

    Order component is being created.
    It will receive order data from useLoaderDate hook, Type for the order data is decleared.

--- src/components/order/Order.tsx
+++ src/components/order/Order.tsx
@@ -1,3 +1,69 @@
-export default function Order() {
-  return <div>Order</div>;
+import { useLoaderData } from "react-router";
+import { calcMinutesLeft, formatCurrency, formatDate } from "../../lib/helpers";
+import SearchOrder from "./SearchOrder";
+
+// Test ID: IIDSAT
+
+type OrderType = {
+  customer: string;
+  status: string;
+  priority: boolean;
+  cart: {
+    addIngredients: string[];
+    removeIngredients: string[];
+    pizzaId: number;
+    name: string;
+    quantity: number;
+    unitPrice: number;
+    totalPrice: number;
+  }[];
+  id: string;
+  estimatedDelivery: string;
+  orderPrice: number;
+  priorityPrice: number;
+};
+
+function Order() {
+  const order = useLoaderData() as OrderType;
+  const {
+    id,
+    status,
+    priority,
+    priorityPrice,
+    orderPrice,
+    estimatedDelivery,
+    cart,
+  } = order;
+  const deliveryIn = calcMinutesLeft(estimatedDelivery);
+
+  return (
+    <div>
+      <SearchOrder />
+      <div>
+        <h2>Status</h2>
+
+        <div>
+          {priority && <span>Priority</span>}
+          <span>{status} order</span>
+        </div>
+      </div>
+
+      <div>
+        <p>
+          {deliveryIn >= 0
+            ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
+            : "Order should have arrived"}
+        </p>
+        <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
+      </div>
+
+      <div>
+        <p>Price pizza: {formatCurrency(orderPrice)}</p>
+        {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
+        <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
+      </div>
+    </div>
+  );
 }
+
+export default Order;
