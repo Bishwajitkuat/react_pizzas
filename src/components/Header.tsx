301d4f4519f870c4c7b8cfada967170ef78e379b
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-01 16:23:40 +0200

    added some more links to Header component.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,21 @@ import React_pizza_logo from "./ui/icons/React_pizza_logo";
 
 export default function Header() {
   return (
-    <header>
+    <header className="flex justify-between items-center px-[5rem] py-[2rem]">
       <Link to="/">
         <React_pizza_logo />
       </Link>
+      <ul className="flex gap-3 text-[1.5rem] font-semibold tracking-widest">
+        <li>
+          <Link to="/menu">Menu</Link>
+        </li>
+        <li>
+          <Link to="/order/new">Order</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+      </ul>
     </header>
   );
 }
