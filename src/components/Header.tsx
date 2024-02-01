74fa18dbde3a819952822ab5640fdd78e0f3a684
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-01 16:09:50 +0200

    React_pizza_log is used in the heading section.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+import React_pizza_logo from "./ui/icons/React_pizza_logo";
 
 export default function Header() {
   return (
     <header>
-      <Link to="/">React Pizza</Link>
+      <Link to="/">
+        <React_pizza_logo />
+      </Link>
     </header>
   );
 }
