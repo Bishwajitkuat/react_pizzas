ebe54e4e500bf647e7a8dc6e60553abcb0f71feb
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-01 16:39:08 +0200

    Loader component is created and loading indicatior is shwon with the help of useNavigation hook.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
 import CartOverview from "./cart/CartOverview";
+import Loader from "./ui/icons/Loader";
 
 export default function Layout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
   return (
     <div>
       <Header />
-      <main>
-        <Outlet />
-      </main>
-      <CartOverview />
+      {isLoading && <Loader />}
+      {!isLoading && (
+        <main>
+          <Outlet />
+        </main>
+      )}
+      {/* <CartOverview /> */}
     </div>
   );
 }
---
+++ src/components/ui/icons/Loader.jsx
@@ -0,0 +1,9 @@
+function Loader() {
+  return (
+    <div>
+      <h1>Loading.....</h1>
+    </div>
+  );
+}
+
+export default Loader;
