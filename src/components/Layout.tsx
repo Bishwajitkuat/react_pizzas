117a28120f7bd3b56e3c3862f7d4717b0424ddfb
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-02 18:08:03 +0200

    Layout component is styled such way that it will always grow to the total screen height.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,21 +1,19 @@
 import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
-import CartOverview from "./cart/CartOverview";
 import Loader from "./ui/icons/Loader";
+import Footer from "./Footer";
 
 export default function Layout() {
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
   return (
-    <div>
+    <div className="flex h-screen w-screen flex-col">
       <Header />
       {isLoading && <Loader />}
-      {!isLoading && (
-        <main>
-          <Outlet />
-        </main>
-      )}
-      {/* <CartOverview /> */}
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 }
