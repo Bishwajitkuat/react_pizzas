d105b9171d9c267f5e743b104227e60d9db977e9
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-02 17:56:39 +0200

    Footer component is being created and styled.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex min-h-[7rem] items-center justify-center bg-orange-400">
+      <p>&copy;Bishwajit Das, 2024</p>
+    </footer>
+  );
+}
