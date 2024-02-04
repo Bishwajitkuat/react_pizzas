a97979443bf9c587ed18fe6d677cbf4671b47c27
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-04 18:38:43 +0200

    OrderItem component is created and styled. It takes each oder item as props and render the name, quantity and subtotal price.

---
+++ src/components/order/OrderItem.tsx
@@ -0,0 +1,11 @@
+export default function OrderItem({ item }) {
+  const { name, quantity, totalPrice } = item;
+  return (
+    <li className="flex justify-between border-b border-t border-zinc-500/20 py-2">
+      <h3>
+        {name} x{quantity}
+      </h3>
+      <p>{totalPrice}€</p>
+    </li>
+  );
+}
