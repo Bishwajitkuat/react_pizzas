42a4b525b0ca4c5490bb6c1fde8fd02a7a3027a1
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-04 17:24:44 +0200

    styling added to CreateOrder component.

--- src/components/order/CreateOrder.tsx
+++ src/components/order/CreateOrder.tsx
@@ -1,7 +1,10 @@
 import { Form, useActionData, useNavigation } from "react-router-dom";
 import { ActionReturnErrorType } from "../../lib/restaurant_api";
+import PersonIcon from "../ui/icons/PersonIcon";
+import MapPinIcon from "../ui/icons/MapPinIcon";
+import PhoneIcon from "../ui/icons/PhoneIcon";
 
-const fakeCart = [
+export const fakeCart = [
   {
     pizzaId: 12,
     name: "Mediterranean",
@@ -33,44 +36,63 @@ export default function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div className="mx-auto flex w-[75%] flex-col items-center gap-4 md:w-[45%]">
-      <h2>Ready to order? Let's go!</h2>
+    <div className="mx-auto flex w-[95%] flex-col items-center gap-4 py-8 md:w-[45%]">
       <Form className="grid w-full gap-4" action="/order/new" method="POST">
         <div className="grid">
-          <label>Name</label>
-          <input
-            className="rounded-md px-4 py-2 outline-none placeholder:text-zinc-400 focus:ring-2 focus:ring-orange-400 focus:ring-opacity-30"
-            type="text"
-            name="customer"
-            placeholder="your full name"
-            required
-          />
+          <div className="flex items-center rounded-full bg-slate-100">
+            <div className="flex h-full items-center justify-center  px-2">
+              <PersonIcon />
+            </div>
+            <label className="min-w-[4.5rem] border-e border-orange-400 py-3 pe-2">
+              Name
+            </label>
+            <input
+              className="w-full rounded-full rounded-s bg-slate-100 px-2 py-3 outline-none placeholder:text-zinc-400 focus:ring-2 focus:ring-orange-400 focus:ring-opacity-30"
+              type="text"
+              name="customer"
+              placeholder="full name"
+              required
+            />
+          </div>
         </div>
 
-        <div className="grid">
-          <label>Phone number</label>
-          <input
-            className="rounded-md px-4 py-2 outline-none placeholder:text-zinc-400 focus:ring-2 focus:ring-orange-400 focus:ring-opacity-30"
-            placeholder="mobile or phone number"
-            type="tel"
-            name="phone"
-            required
-          />
+        <div className="grid gap-2">
+          <div className="flex items-center rounded-full bg-slate-100">
+            <div className="flex h-full items-center justify-center px-2">
+              <PhoneIcon />
+            </div>
+            <label className="min-w-[4.5rem] border-e border-orange-400 py-3 pe-2">
+              Number
+            </label>
+            <input
+              className="w-full rounded-full rounded-s bg-slate-100 px-2 py-3 outline-none placeholder:text-zinc-400 focus:ring-2 focus:ring-orange-400 focus:ring-opacity-30"
+              placeholder="contact number"
+              type="tel"
+              name="phone"
+              required
+            />
+          </div>
           {actionData?.phone && (
             <p className="text-red-500">{actionData.phone}</p>
           )}
         </div>
 
-        <div className="grid">
-          <label>Address</label>
-
-          <input
-            className="rounded-md px-4 py-2 outline-none placeholder:text-zinc-400 focus:ring-2 focus:ring-orange-400 focus:ring-opacity-30"
-            placeholder="address"
-            type="text"
-            name="address"
-            required
-          />
+        <div className="grid gap-2">
+          <div className="flex items-center rounded-full bg-slate-100 ">
+            <div className="flex h-full items-center justify-center  px-2">
+              <MapPinIcon />
+            </div>
+            <label className="min-w-[4.5rem] border-e border-orange-400 py-3 pe-2">
+              Address
+            </label>
+            <input
+              className="w-full rounded-full rounded-s bg-slate-100 px-2 py-3 outline-none placeholder:text-zinc-400 focus:ring-2 focus:ring-orange-400 focus:ring-opacity-30"
+              placeholder="delivary address"
+              type="text"
+              name="address"
+              required
+            />
+          </div>
         </div>
 
         <div>
@@ -93,7 +115,7 @@ export default function CreateOrder() {
             value={JSON.stringify(cart)}
           />
           <button
-            className="w-full rounded-xl bg-orange-400  px-4 py-2 font-semibold shadow-md shadow-zinc-500 outline-none duration-200 ease-in  hover:bg-orange-500 "
+            className="min-h-[3rem] w-full rounded-full bg-orange-300 px-4  py-2 font-semibold uppercase shadow-sm shadow-zinc-500 outline-none duration-200 ease-in  hover:bg-orange-500 "
             disabled={isSubmitting}
           >
             {isSubmitting ? "Submitting....." : "Order now"}
