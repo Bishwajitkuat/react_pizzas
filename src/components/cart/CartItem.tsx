c5fd67e14ec799dfcb6851a0563a12e27cd70c69
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-05 14:35:48 +0200

    Image added for each pizza item in cart and some styling adjusment to fit the image in mobile and desktop view.

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -8,21 +8,22 @@ import {
 
 export default function CartItem({ cartItem }) {
   const dispatch = useDispatch();
-  const { id, name, quantity, unitPrice, totalPrice } = cartItem;
+  const { id, name, quantity, unitPrice, totalPrice, imageUrl } = cartItem;
   return (
-    <li className="grid w-full grid-cols-5 rounded-md bg-slate-100 p-4 shadow-md shadow-zinc-200 duration-150 ease-in  hover:bg-orange-300 hover:shadow-orange-300/50">
-      <div className="col-span-2 md:col-span-3">
+    <li className="grid w-full grid-cols-5 items-center gap-1 rounded-md bg-slate-100 p-3 shadow-md shadow-zinc-200 duration-150 ease-in hover:bg-orange-300  hover:shadow-orange-300/50 md:gap-4 md:p-4">
+      <img className=" rounded-md" src={imageUrl} alt={name} />
+      <div className="col-span-2">
         <h2 className="tracking-widest md:text-xl">{name}</h2>
         <p>{unitPrice}€</p>
       </div>
-      <div className="col-span-2 grid grid-cols-3 items-center justify-center gap-2 justify-self-center md:col-span-1 md:text-xl md:font-bold">
+      <div className="grid grid-cols-3 items-center justify-center gap-2 justify-self-center md:col-span-1 md:text-xl md:font-bold">
         <button
           className=" hover:text-green-500"
           onClick={() => dispatch(addOneToCart(id))}
         >
           +
         </button>
-        <p className="rounded-md border border-zinc-600 px-2">{quantity}</p>
+        <p>{quantity}</p>
         <button
           className=" hover:text-red-500"
           onClick={() => {
@@ -32,12 +33,12 @@ export default function CartItem({ cartItem }) {
           -
         </button>
       </div>
-      <div className="grid grid-cols-2 items-center justify-center gap-2 justify-self-end pe-2">
+      <div className="grid grid-cols-2 items-center justify-center gap-2 justify-self-end">
         <p className="md:text-xl md:font-bold md:tracking-widest">
           {totalPrice}€
         </p>
         <button
-          className="duration-200 ease-in hover:scale-105"
+          className="ms-2 duration-200 ease-in hover:scale-105"
           onClick={() => dispatch(removeFromCart(id))}
         >
           <TrashCanIcon />
