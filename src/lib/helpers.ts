e3e0c5ce6725c577a1b85048794d3e876bc4a3ef
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-05 13:34:17 +0200

    Declearing types

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,11 +1,11 @@
-export function formatCurrency(value) {
+export function formatCurrency(value: number): string {
   return new Intl.NumberFormat("en", {
     style: "currency",
     currency: "EUR",
   }).format(value);
 }
 
-export function formatDate(dateStr) {
+export function formatDate(dateStr: string): string {
   return new Intl.DateTimeFormat("en", {
     day: "numeric",
     month: "short",
@@ -14,7 +14,7 @@ export function formatDate(dateStr) {
   }).format(new Date(dateStr));
 }
 
-export function calcMinutesLeft(dateStr) {
+export function calcMinutesLeft(dateStr: string): number {
   const d1 = new Date().getTime();
   const d2 = new Date(dateStr).getTime();
   return Math.round((d2 - d1) / 60000);
