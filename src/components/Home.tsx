619e16a5d3a96f404bf12c35a55ad794b32b15bf
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-05 19:01:46 +0200

    pizza.jpg moved to public directory and correcting path to it.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,7 @@ import LinkButton from "./ui/LinkButton";
 
 export default function Home() {
   return (
-    <div className="flex h-full w-full flex-col items-center  bg-[url(src/assets/pizza.jpg)] bg-center bg-no-repeat pt-[3rem] text-center ">
+    <div className="flex h-full w-full flex-col items-center  bg-[url(/pizza.jpg)] bg-center bg-no-repeat pt-[3rem] text-center ">
       <div className=" grid gap-[2rem] rounded-lg bg-zinc-400/50 p-[4rem] text-zinc-950 shadow-xl shadow-orange-200/40 md:bg-zinc-400/30 md:text-zinc-800 ">
         <h1 className=" text-[1.75rem] md:text-[4rem]  ">
           React Pizza is the best!!!
