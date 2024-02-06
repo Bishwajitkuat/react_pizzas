e86f4bd5d4b5eeb7ec325ff938248b23d0062bd6
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-06 17:15:39 +0200

    Implements sticky head section.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,9 +8,11 @@ export default function Layout() {
   const isLoading = navigation.state === "loading";
   return (
     <div className="flex h-screen w-screen min-w-[360px] flex-col">
-      <Header />
+      <div className="">
+        <Header />
+      </div>
       {isLoading && <Loader />}
-      <main className="grow bg-gradient-to-br from-zinc-300 to-zinc-900">
+      <main className="flex-1 grow overflow-y-auto bg-gradient-to-br from-zinc-300 to-zinc-900">
         <Outlet />
       </main>
       <Footer />
